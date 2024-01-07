b2ceb7b82d4a69647917e97fb140b3778e44ec09
Googol Lee <i@googol.im>
2024-01-07 11:26:26 +0100

    Fix data

--- src/main.go
+++ src/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"vdbbench/client"
 
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/graphql"
+	"github.com/weaviate/weaviate/entities/models"
 )
 
 func main() {
@@ -14,47 +16,57 @@ func main() {
 		panic(err)
 	}
 
-	// classObj := &models.Class{
-	// Class: "Question",
-	// }
+	classObj := &models.Class{
+		Class: "Question",
+	}
 
-	// // add the schema
-	// err = client.Schema().ClassCreator().WithClass(classObj).Do(context.Background())
-	// if err != nil {
-	// panic(err)
-	// }
+	// add the schema
+	err = client.Schema().ClassCreator().WithClass(classObj).Do(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// items := []struct {
-	// Category string
-	// Question string
-	// Answer   string
-	// }{
-	// {"SCIENCE", "This organ removes excess glucose from the blood & stores it as glycogen", "Liver"},
-	// {"ANIMALS", "It's the only living mammal in the order Proboseidea", "Elephant"},
-	// {"SCIENCE", "In 70-degree air, a plane traveling at about 1,130 feet per second breaks it", "Sound barrier"},
-	// }
-	// objects := make([]*models.Object, len(items))
-	// for i := range items {
-	// objects[i] = &models.Object{
-	// Class: "Question",
-	// Properties: map[string]any{
-	// "category": items[i].Category,
-	// "question": items[i].Question,
-	// "answer":   items[i].Answer,
-	// },
-	// Vector: []float32{0.1, 0.4, 0.2},
-	// }
-	// }
+	defer func() {
+		err = client.Schema().ClassDeleter().
+			WithClassName("Question").
+			Do(context.Background())
 
-	// batchRes, err := client.Batch().ObjectsBatcher().WithObjects(objects...).Do(context.Background())
-	// if err != nil {
-	// panic(err)
-	// }
-	// for _, res := range batchRes {
-	// if res.Result.Errors != nil {
-	// panic(res.Result.Errors.Error)
-	// }
-	// }
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	items := []struct {
+		Category string
+		Question string
+		Answer   string
+	}{
+		{"SCIENCE", "This organ removes excess glucose from the blood & stores it as glycogen", "Liver"},
+		{"ANIMALS", "It's the only living mammal in the order Proboseidea", "Elephant"},
+		{"SCIENCE", "In 70-degree air, a plane traveling at about 1,130 feet per second breaks it", "Sound barrier"},
+	}
+	objects := make([]*models.Object, len(items))
+	for i := range items {
+		objects[i] = &models.Object{
+			Class: "Question",
+			Properties: map[string]any{
+				"category": items[i].Category,
+				"question": items[i].Question,
+				"answer":   items[i].Answer,
+			},
+			Vector: []float32{0.1, 0.4, 0.2},
+		}
+	}
+
+	batchRes, err := client.Batch().ObjectsBatcher().WithObjects(objects...).Do(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, res := range batchRes {
+		if res.Result.Errors != nil {
+			panic(res.Result.Errors.Error)
+		}
+	}
 
 	fields := []graphql.Field{
 		{Name: "question"},
@@ -65,7 +77,7 @@ func main() {
 	near := client.GraphQL().
 		NearVectorArgBuilder().
 		WithDistance(0.2).
-		WithVector([]float32{0.3})
+		WithVector([]float32{0.1, 0.3, 0.2})
 
 	result, err := client.GraphQL().Get().
 		WithClassName("Question").
@@ -74,9 +86,8 @@ func main() {
 		WithLimit(2).
 		Do(context.Background())
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	fmt.Printf("%v\n", result.Errors[0].Message)
 	fmt.Printf("%+v\n", result)
 }
