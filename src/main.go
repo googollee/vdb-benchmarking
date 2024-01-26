a48efb66f499076047121e79ecff4465512ffe6f
Googol Lee <i@googol.im>
2024-01-26 21:12:36 +0100

    Add qdrant.

--- src/main.go
+++ src/main.go
@@ -29,20 +29,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Time(ctx, "import:", func() {
-	// f, err := os.Open("./data/sources.json")
-	// if err != nil {
-	// log.Fatalf("open error: %v", err)
-	// }
-	// defer f.Close()
+	Time(ctx, "import:", func() {
+		f, err := os.Open("./data/sources.json")
+		if err != nil {
+			log.Fatalf("open error: %v", err)
+		}
+		defer f.Close()
 
-	// buff := bufio.NewReader(f)
-	// decoder := json.NewDecoder(buff)
+		buff := bufio.NewReader(f)
+		decoder := json.NewDecoder(buff)
 
-	// if err := vdb.Import(ctx, decoder); err != nil {
-	// log.Fatal(err)
-	// }
-	// })
+		if err := vdb.Import(ctx, decoder); err != nil {
+			log.Fatal(err)
+		}
+	})
 
 	Time(ctx, "query:", func() {
 		f, err := os.Open("./data/queries.json")
